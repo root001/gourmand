e31c397d7e456f0727cab7dc5cbe40551897fd62
Abdul-Basit <adebayoabdulbasit@gmail.com>
2025-08-20 23:30:01 +0100

    landing page and project setup

---
+++ app/(pages)/_layout.tsx
@@ -0,0 +1,9 @@
+import {Slot, Redirect} from 'expo-router';
+
+export default function _Layout (){
+    const isAuthenticated = false;
+
+    if(!isAuthenticated) return <Redirect href="/sign-in" />
+
+    return <Slot />
+}
